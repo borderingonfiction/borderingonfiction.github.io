cf15b0301dae7d44880e6d6d9666f9bfc261b3c8
moon <amf337@cornell.edu>
2026-09-01 03:07:05 -0400

    afjlksdjflsf

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import { createRoot } from 'react-dom/client';
-import {renderNav} from '.components.js'; 
+import {renderNav} from './components.js'; 
 
 const navDomNode = document.getElementById('navMenu');
 const navRoot = createRoot(navDomNode);
