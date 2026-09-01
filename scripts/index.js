e59f49362ca107ac0199fe0e14e88fbf6fdfdfc1
moon <amf337@cornell.edu>
2026-09-01 02:10:42 -0400

    dawg idek

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  
+ <App/>  
+  ,
+  document.getElementById('root')
+);
