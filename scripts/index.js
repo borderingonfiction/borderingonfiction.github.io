71250311c4b7a117c7f58622dbf170a7ab38e098
moon <amf337@cornell.edu>
2026-09-03 14:17:42 -0400

    shoot me istg

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,15 @@
 
 $(document).ready(function () {
     $('#navMenuPlus').on('click', function () {
-        $('#navMenu').slideToggle();
-        $('#navMenuMinus').toggle();
-        $('#navMenuPlus').toggle();
+        $('#navMenu').slideToggle()
+        $('#navMenuMinus').toggle()
+        $('#navMenuPlus').toggle()
     });
 
     $('#navMenuMinus').on('click', function () {
-        $('#navMenu').slideToggle();
-        $('#navMenuMinus').toggle();
-        $('#navMenuPlus').toggle();
+        $('#navMenu').slideToggle()
+        $('#navMenuMinus').toggle()
+        $('#navMenuPlus').toggle() 
     });
 });
 
