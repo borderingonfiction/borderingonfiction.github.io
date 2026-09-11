00d0816b7efa7a5fac861f0524bc354da4b3f9ea
moon <amf337@cornell.edu>
2026-09-11 12:44:42 -0400

    well no clue

--- scripts/index.js
+++ scripts/index.js
@@ -22,9 +22,23 @@ $(document).ready(function () {
 
 });
 
+
+// wait for the DOM to be loaded
+
+$(function () {
+    // bind 'myForm' and provide a simple callback function
+    $('#guestbook').ajaxForm(function () {
+        alert("thank you and take care!");
+    });
+});
+
 var form = $('#guestbook').ajaxSubmit({ /* options */ });
 var xhr = form.data('jqxhr');
 
 xhr.done(function () {
 });
 
+$.ajax({
+    dataType: "json",
+    data: FormData,
+}
